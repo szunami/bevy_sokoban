bda155a794aa0c9387640957c6870c027931a0cf
Sam Szuflita <sam.szuflita@gmail.com>
2020-11-05 10:39:01 -0500

    Add grid location

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(player_movement_system.system())
-        // .add_system(greet_people.system())
+        .add_system(render_grid_location_to_transform.system())
         .run();
 }
 
 struct Player;
 
 struct Box;
+
+struct GridLocation(i32, i32);
 
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands
@@ -24,6 +26,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
+        .with(GridLocation(0, 0))
         .with(Player)
 
 
@@ -33,27 +36,31 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
+        .with(GridLocation(-5, 0))
         .with(Box);
 }
 
 fn player_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut Transform)>,
+    mut query: Query<(&Player, &mut GridLocation)>,
 ) {
-    for (_player, mut transform) in query.iter_mut() {
+    for (_player, mut grid_location) in query.iter_mut() {
         // can only move in one direction per step
         // debounce movement in each direction
         if keyboard_input.just_pressed(KeyCode::Left) {
-            let translation = &mut transform.translation;
-            // move the paddle horizontally
-            *translation.x_mut() -= 10.0;
-            *translation.x_mut() = translation.x().min(300.0).max(-300.0);
+            grid_location.0 -= 1;
         }
         if keyboard_input.just_pressed(KeyCode::Right) {
-            let translation = &mut transform.translation;
-            // move the paddle horizontally
-            *translation.x_mut() += 10.0;
-            *translation.x_mut() = translation.x().min(300.0).max(-300.0);
+            grid_location.0 += 1;
         }
+    }
+}
+
+fn render_grid_location_to_transform(
+    mut query: Query<(&GridLocation, &mut Transform)>,
+) {
+    for (grid_location, mut transform) in query.iter_mut() {
+        *transform.translation.x_mut() = 10. * grid_location.0 as f32;
+        *transform.translation.y_mut() = 10. * grid_location.1 as f32;
     }
 }
