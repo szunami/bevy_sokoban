f235878aa4eee2fc2b8a02bf49ef355d05e701c4
Sam Szuflita <sam.szuflita@gmail.com>
2020-11-06 10:25:56 -0500

    QuerySet impl works

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ struct Player;
 
 struct Box;
 
+struct Movable;
+
 struct Wall;
 
 #[derive(Hash, Eq, PartialEq, Debug)]
@@ -32,6 +34,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             ..Default::default()
         })
         .with(GridLocation(0, 0))
+        .with(Movable)
         .with(Player);
 
     commands
@@ -42,6 +45,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             ..Default::default()
         })
         .with(GridLocation(-5, 0))
+        .with(Movable)
         .with(Box);
 
     commands
@@ -52,6 +56,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             ..Default::default()
         })
         .with(GridLocation(-7, 0))
+        .with(Movable)
         .with(Box);
 
     commands
@@ -72,6 +77,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             ..Default::default()
         })
         .with(GridLocation(5, 0))
+        .with(Movable)
         .with(Box);
 
     commands
@@ -82,6 +88,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             ..Default::default()
         })
         .with(GridLocation(7, 0))
+        .with(Movable)
         .with(Box);
 
     commands
@@ -97,58 +104,65 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
 
 fn player_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut box_query: Query<(Entity, &Box, &mut GridLocation)>,
-    wall_query: Query<(Entity, &Wall, &GridLocation)>,
-    mut people_query: Query<(Entity, &Player, &mut GridLocation)>,
+    mut wall_query: Query<(Entity, &Wall, &GridLocation)>,
+    mut set: QuerySet<(
+        Query<(Entity, &Movable, &mut GridLocation)>,
+        Query<(Entity, &Player, &GridLocation)>,
+    )>,
 ) {
+    let mut delta = 0;
+    if keyboard_input.just_pressed(KeyCode::Left) {
+        delta = -1;
+    }
+    if keyboard_input.just_pressed(KeyCode::Right) {
+        delta = 1;
+    }
+    if delta == 0 {
+        return;
+    }
+
     let immovables: HashMap<GridLocation, Entity> = {
         let mut tmp = HashMap::new();
-
-        for (box_entity, _b, grid_location) in wall_query.iter() {
-            tmp.insert(GridLocation(grid_location.0, grid_location.1), box_entity);
+        for (wall_entity, _wall, wall_grid_location) in wall_query.iter_mut() {
+            tmp.insert(
+                GridLocation(wall_grid_location.0, wall_grid_location.1),
+                wall_entity,
+            );
         }
         tmp
     };
 
     let movables: HashMap<GridLocation, Entity> = {
         let mut tmp = HashMap::new();
-
-        for (box_entity, _b, grid_location) in box_query.iter_mut() {
-            tmp.insert(GridLocation(grid_location.0, grid_location.1), box_entity);
+        for (movable_entity, _movable, grid_location) in set.q0_mut().iter_mut() {
+            tmp.insert(
+                GridLocation(grid_location.0, grid_location.1),
+                movable_entity,
+            );
         }
         tmp
     };
 
-    for (_e, _player, mut grid_location) in people_query.iter_mut() {
-        let mut to_move: Vec<Entity> = vec![];
-        let mut new_x = grid_location.0;
-        let new_y = grid_location.1;
-        let mut delta = 0;
-        // can only move in one direction per step
-        // debounce movement in each direction
-        if keyboard_input.just_pressed(KeyCode::Left) {
-            delta = -1;
-        }
-        if keyboard_input.just_pressed(KeyCode::Right) {
-            delta = 1;
-        }
-        if delta == 0 {
-            continue;
-        }
-        new_x += delta;
+    let mut to_move: Vec<Entity> = vec![];
 
-        while let Some(movable) = movables.get(&GridLocation(new_x, new_y)) {
-            to_move.push(*movable);
-            new_x += delta;
+    for (player_entity, _player, player_grid_location) in set.q1().iter() {
+        let mut tmp_to_move = vec![];
+
+        let mut current_x = player_grid_location.0;
+        let current_y = player_grid_location.1;
+        while let Some(movable) = movables.get(&GridLocation(current_x, current_y)) {
+            tmp_to_move.push(*movable);
+            current_x += delta;
         }
-        if let Some(_immovable) = immovables.get(&GridLocation(new_x, new_y)) {
+        if let Some(_immovable) = immovables.get(&GridLocation(current_x, current_y)) {
             continue;
         }
+        to_move.append(&mut tmp_to_move);
+    }
+
+    for loc in to_move {
+        let mut grid_location: Mut<GridLocation> = set.q0_mut().get_component_mut(loc).unwrap();
         grid_location.0 += delta;
-        for loc in to_move {
-            let mut grid_location: Mut<GridLocation> = box_query.get_component_mut(loc).unwrap();
-            grid_location.0 += delta;
-        }
     }
 }
 
